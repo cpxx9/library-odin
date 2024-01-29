18f06cda2fea87b43116152781c63c836a603448
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-29 11:54:03 -0500

    added button to delete book, finished styling for buuttons

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,27 +1,28 @@
-const myLibrary = [];
-let test = new Book("hobbit", "jrr tolkien", "290", false);
-let test2 = new Book("potter", "jk rowling", "400", true);
-let test3 = new Book("potter", "jk rowling", "400", true);
-let test4 = new Book("potter", "jk rowling", "400", true);
-let test5 = new Book("potter", "jk rowling", "400", true);
-let test6 = new Book("potter", "jk rowling", "400", true);
-let test7 = new Book("potter", "jk rowling", "400", true);
-let test8 = new Book("potter", "jk rowling", "400", true);
-let test9 = new Book("potter", "jk rowling", "400", true);
-let test10 = new Book("potter", "jk rowling", "400", true);
-let test11 = new Book("potter", "jk rowling", "400", true);
+// let test = new Book("hobbit", "jrr tolkien", "290", false);
+// let test2 = new Book("potter", "jk rowling", "400", true);
+// let test3 = new Book("potter", "jk rowling", "400", true);
+// let test4 = new Book("potter", "jk rowling", "400", true);
+// let test5 = new Book("potter", "jk rowling", "400", true);
+// let test6 = new Book("potter", "jk rowling", "400", true);
+// let test7 = new Book("potter", "jk rowling", "400", true);
+// let test8 = new Book("potter", "jk rowling", "400", true);
+// let test9 = new Book("potter", "jk rowling", "400", true);
+// let test10 = new Book("potter", "jk rowling", "400", true);
+// let test11 = new Book("potter", "jk rowling", "400", true);
 
-myLibrary.push(test);
-myLibrary.push(test2);
-myLibrary.push(test3);
-myLibrary.push(test4);
-myLibrary.push(test5);
-myLibrary.push(test6);
-myLibrary.push(test7);
-myLibrary.push(test8);
-myLibrary.push(test9);
-myLibrary.push(test10);
-myLibrary.push(test11);
+// myLibrary.push(test);
+// myLibrary.push(test2);
+// myLibrary.push(test3);
+// myLibrary.push(test4);
+// myLibrary.push(test5);
+// myLibrary.push(test6);
+// myLibrary.push(test7);
+// myLibrary.push(test8);
+// myLibrary.push(test9);
+// myLibrary.push(test10);
+// myLibrary.push(test11);
+const myLibrary = [];
+let newBookCard = document.createElement('div');
 const cardGrid = document.querySelector('.book-card-area');
 
 function Book(title, author, pages, hasRead) {
@@ -40,11 +41,12 @@ function addBookToLibrary() {
 
 function displayBooks() {
   myLibrary.forEach((book) => {
-    let newBookCard = document.createElement('div');
     newBookCard.innerHTML = `<h2>${book.title}</h2>
                               <h3>${book.author}</h3>
                               <h4>${book.pages}</h4>
-                              <p>${book.hasRead ? "You have read this" : "You have not read this"}</p>`;
+                              <p>${book.hasRead ? "You have read this" : "You have not read this"}</p>
+                              <button type='button' class='btn change-hasRead'>Toggle whether you have read this book</button>
+                              <button type='button' class='btn delete-book'>-</button>`;
     newBookCard.classList.add('book-card');
     cardGrid.appendChild(newBookCard);
   });

ef48172be22469cbeb7889cb4032a0f4dec4ae57
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2024-01-29 11:19:11 -0500

    edited book card grid layout

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,27 @@
 const myLibrary = [];
 let test = new Book("hobbit", "jrr tolkien", "290", false);
+let test2 = new Book("potter", "jk rowling", "400", true);
+let test3 = new Book("potter", "jk rowling", "400", true);
+let test4 = new Book("potter", "jk rowling", "400", true);
+let test5 = new Book("potter", "jk rowling", "400", true);
+let test6 = new Book("potter", "jk rowling", "400", true);
+let test7 = new Book("potter", "jk rowling", "400", true);
+let test8 = new Book("potter", "jk rowling", "400", true);
+let test9 = new Book("potter", "jk rowling", "400", true);
+let test10 = new Book("potter", "jk rowling", "400", true);
+let test11 = new Book("potter", "jk rowling", "400", true);
 
 myLibrary.push(test);
+myLibrary.push(test2);
+myLibrary.push(test3);
+myLibrary.push(test4);
+myLibrary.push(test5);
+myLibrary.push(test6);
+myLibrary.push(test7);
+myLibrary.push(test8);
+myLibrary.push(test9);
+myLibrary.push(test10);
+myLibrary.push(test11);
 const cardGrid = document.querySelector('.book-card-area');
 
 function Book(title, author, pages, hasRead) {
